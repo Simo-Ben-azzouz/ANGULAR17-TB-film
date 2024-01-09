ad7c6688ff3bd65a2dfa33fdf07a44865ed476a3
simo <simo@simos-MacBook-Pro.local>
2024-01-09 19:03:02 +0100

    AddFormLogin

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { BG_IMG_URL, LOGO_URL } from '../../constant/config';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     CommonModule,
-    MatCardModule,
+    FormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
@@ -17,6 +17,18 @@ export class LoginComponent {
   logoUrl = LOGO_URL;
   bgUrl = BG_IMG_URL;
 
-  
-}
+  email!:string;
+  password!:string;
 
+  onSubmit()
+  {
+    // Validation password and email
+    if (this.email || this.password)
+    {
+      alert("provide email or password")  
+      return ;
+    }
+    // V & P are correct => login
+    
+  }
+}
