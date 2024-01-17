3992902189e92a0898d807304ca51972034344c8
simo <simo@simos-MacBook-Pro.local>
2024-01-17 21:30:24 +0100

    add-newRouting-dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
 import { BrowseComponent } from './Pages/browse/browse.component';
 import { authGuard } from './guards/auth.guard';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
         path : "login",
         component : LoginComponent
+    },
+    {
+        path : "dashboard",
+        component : DashboardComponent
     },
     {
         path : "browse",
