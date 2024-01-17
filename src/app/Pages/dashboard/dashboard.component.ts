fc01d0b395743dc60814ddbe1e06754850d65d6c
simo <simo@simos-MacBook-Pro.local>
2024-01-17 21:31:49 +0100

    start-build-dashboard

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { DHeaderComponent } from '../../components/dashboard/d-header/d-header.component';
+import { DMainComponent } from '../../components/dashboard/d-main/d-main.component';
+import { DSideNavComponent } from '../../components/dashboard/d-side-nav/d-side-nav.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DHeaderComponent,DMainComponent,DSideNavComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
