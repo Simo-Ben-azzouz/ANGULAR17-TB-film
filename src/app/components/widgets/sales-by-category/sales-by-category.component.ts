641f1ea95cd8c06fac38720d25c4794795f89488
simo <simo@simos-MacBook-Pro.local>
2024-01-19 12:41:05 +0100

    add-PieChart-GenreByFilm

--- src/app/components/widgets/sales-by-category/sales-by-category.component.ts
+++ src/app/components/widgets/sales-by-category/sales-by-category.component.ts
@@ -1,12 +1,75 @@
 import { Component } from '@angular/core';
+import { Chart, ChartModule } from 'angular-highcharts';
 
 @Component({
   selector: 'app-sales-by-category',
   standalone: true,
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './sales-by-category.component.html',
-  styleUrl: './sales-by-category.component.css'
+  styleUrl: './sales-by-category.component.scss'
 })
 export class SalesByCategoryComponent {
+  chart = new Chart({
+    chart: {
+      type: 'pie',
+      height: 325
+    },
+    title: {
+      text: 'Genre'
+    },
+    xAxis: {
+      categories: [
+        'Action',
+        'Adventure',
+        'Comedy',
+        'Drama',
+        'Fantasy',
+      ]
+    },
+    yAxis: {
+      title: {
+        text: 'Revenue in %'
+      }
+    },
+    series: [
+     {
+      type: 'pie',
+      data: [
+        {
+          name: 'Action',
+          y: 41.0,
+          color: '#CE1212',
+        },
+        {
+          name: 'Adventure',
+          y: 33.8,
+          color: '#555273',
+        },
+        {
+          name: 'Comedy',
+          y: 6.5,
+          color: '#EEEBDD',
+        },
+        {
+          name: 'Drama',
+          y: 15.2,
+          color: '#E23E57',
+        },
+        {
+          name: 'Fantasy',
+          y: 3.5,
+          color: '#810000',
+        },
+      ]
+     }
+    ],
+    credits: {
+      enabled: false
+    }
+  }as any)
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
